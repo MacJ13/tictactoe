0f57e7c9db239fb6b06b5098da3f9e491c76506b
MacJ13 <jacdim02@gmail.com>
2023-03-08 10:52:08 +0100

    Create 'checkLineupBoard' function
    inside gameBoard module pattern

--- app.js
+++ app.js
@@ -44,12 +44,37 @@ const gameBoard = (function () {
     return board[index] === "";
   }
 
+  // check possible correct lines
+  function checkLineupBoard(mark) {
+    const correctLineups = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const lineup of correctLineups) {
+      // const [a, b, c] = lineup;
+      // console.log(lineup, [board[a], board[b], board[c]]);
+      if (lineup.every((index) => board[index] === mark)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   return {
     getBoard,
     addMarkToBoard,
     isBoardFilled,
     clearBoard,
     isElementBoardEmpty,
+    checkLineupBoard,
   };
 })();
 
